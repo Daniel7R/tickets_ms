138e0b33b012ce874155ca42523d8335a9b42a08
Daniel Rivera <daniel.riverar@pragma.com.co>
2025-03-18 15:31:33 -0500

    feature config docker file

--- Infrastructure/Filters/ApiKeyFilter.cs
+++ Infrastructure/Filters/ApiKeyFilter.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using TicketsMS.Application.DTOs.Request;
 
 namespace TicketsMS.Infrastructure.Filters
 {
@@ -13,7 +14,7 @@ namespace TicketsMS.Infrastructure.Filters
             var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
             var EXPECTED = config["APIKEY:TICKETS"];
             if (string.IsNullOrEmpty(EXPECTED) || !request.Headers.TryGetValue(API_KEY_HEADER_NAME, out var provided) || provided != EXPECTED)
-                context.Result = new UnauthorizedObjectResult(new { Message="API KEY is missing or invalid" });
+                context.Result = new UnauthorizedObjectResult(new ResponseDTO<string?>{ Message="API KEY is missing or invalid" });
         }
     }
 }
